694b49daba9f0c4e52a156549328cd36b22703d9
Aliya Petranik <petranik@usc.edu>
2020-03-01 21:27:38 -0800

    updated emergency numbers

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -23,6 +23,7 @@ export default function Home () {
             <text>Devpost</text>
             <text>Slack</text>
             <text>Emergency</text>
+            <text>Non-Emergency</text>
             <text>MLH Code of Conduct</text>
           </div>
           <div className='welcome-column'>
@@ -37,7 +38,9 @@ export default function Home () {
             </text>
             <a target='_blank' rel='noopener noreferrer' href="https://athenahacks2020.devpost.com/">athenahacks2020.devpost.com</a>
             <a target='_blank' rel='noopener noreferrer' href='http://bit.ly/athenahacks2020slack'>bit.ly/athenahacks2020slack</a>
-            <text>(213) 740-6000</text>
+            <text>(213)-740-4321</text>
+            <text>(213)-740-6000</text>
+
             <a target='_blank' rel='noopener noreferrer' href='https://static.mlh.io/docs/mlh-code-of-conduct.pdf'>mlh-code-of-conduct.pdf</a>
 
           </div>
